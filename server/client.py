1f932c41452cf532fc23e73c62bcea5b1bc65785
Sandra <sandra@ubuntu.(none)>
2017-03-04 15:10:38 -0500

    add client code

---
+++ server/client.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python           # This is client.py file
+
+import socket               # Import socket module
+
+s = socket.socket()         # Create a socket object
+#host = socket.gethostname() # Get local machine name
+host = "192.168.2.3"
+port = 12345                # Reserve a port for your service.
+
+s.connect((host, port))
+#print s.recv(1024)
+while True:
+    print s.recv(1024)
+s.close                     # Close the socket when done
